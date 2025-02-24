20dee0bd8fa39f36c27c8110d6001a0a12c04f6e
abdulrahman berhan <abdulrahmanberhan@Mac.lan>
2025-02-23 22:39:06 -0700

    fixed mobile footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,33 +4,29 @@ import { FaLinkedin, FaGithub } from "react-icons/fa"; // Import social icons
 const Footer = () => {
   return (
     <footer className="footer">
-      {/* Top Section - Button on Left, Social Icons on Right */}
+      {/* Top Row: Contact Button & Social Icons */}
       <div className="footer-top">
-        <div className="footer-left">
-          <button 
-            className="contact-btn" 
-            onClick={() => window.location.href = 'mailto:berh3n@gmail.com?subject=Hey%20Berhan!&body=I%20wanted%20to%20reach%20out%20regarding...'}
-          >
-            Contact Me <span className="icon">📧</span>
-          </button>
+        <button 
+          className="contact-btn" 
+          onClick={() => window.location.href = 'mailto:berh3n@gmail.com?subject=Hey%20Berhan!&body=I%20wanted%20to%20reach%20out%20regarding...'}
+        >
+          Contact Me <span className="icon">📧</span>
+        </button>
 
-           {/* Middle Row: Centered Paragraph */}
-      <p className="footer-text">I like building things that make 💰!</p>
-        </div>
-
-        <div className="footer-right">
+        <div className="footer-socials">
           <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="social-icon">
-            <FaLinkedin size={32} color="white"/>
+            <FaLinkedin size={28} color="white"/>
           </a>
           <a href="https://github.com/berhancodes" target="_blank" rel="noopener noreferrer" className="social-icon">
-            <FaGithub size={32} color="white" />
+            <FaGithub size={28} color="white" />
           </a>
         </div>
       </div>
 
-     
+      {/* Centered Paragraph */}
+      <p className="footer-text">I like building things that make 💰!</p>
 
-      {/* Bottom Row: Centered Copyright */}
+      {/* Copyright Row */}
       <div className="footer-bottom">
         <p>&copy; {new Date().getFullYear()} HeyItsBerhan. All rights reserved.</p>
       </div>
